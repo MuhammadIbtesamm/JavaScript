f6fd6b5f3ac48da7dba47d568feb01e83c74d155
Ibtesam <128898788+MuhammadIbtesamm@users.noreply.github.com>
2024-07-11 15:30:00 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,29 @@
+//Print Hello World
+
+console.log("Hello World");
+
+//Variables
+
+let FullName = "Ibtesam";
+console.log(FullName);
+let Age = 21;
+console.log(Age);
+let isStudent = true;
+console.log(isStudent);
+
+//Types of Variables
+
+console.log(typeof FullName);
+console.log(typeof Age);
+console.log(typeof isStudent);
+
+//Objects
+const person = {
+    FullName: "Ibtesam",
+    Age: 21,
+    isStudent: true
+}
+
+//For Changing in Object
+person.FullName = "Ibtesam Khaliq";
+console.log(person.FullName);
